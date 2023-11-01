faf9c52d0c2ee9847941c10e6d7cb63488d841fb
François LEIBER <fleiber@gmail.com>
2023-11-01 20:28:05 +0100

    Upgrade dependencies, including Kotlin 1.9.20, Gradle 8.4 and Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    id("com.github.ben-manes.versions") version "0.47.0" // find dependency updates
+    kotlin("jvm") version "1.9.20"
+    id("com.github.ben-manes.versions") version "0.49.0" // find dependency updates
 }
 
 repositories {
@@ -13,27 +13,27 @@ repositories {
 }
 
 dependencies {
-    api("com.google.guava:guava:32.1.1-jre")
+    api("com.google.guava:guava:32.1.3-jre")
     api("org.apache.commons:commons-csv:1.10.0")
 
-    implementation("ch.qos.logback:logback-classic:1.4.8")
-    implementation("com.github.luben:zstd-jni:1.5.5-5")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("com.github.luben:zstd-jni:1.5.5-10")
 
-    implementation("com.itextpdf:kernel:8.0.0")
-    implementation("com.itextpdf:io:8.0.0")
-    implementation("com.itextpdf:layout:8.0.0")
-    implementation("com.itextpdf:bouncy-castle-adapter:8.0.0")
+    implementation("com.itextpdf:kernel:8.0.2")
+    implementation("com.itextpdf:io:8.0.2")
+    implementation("com.itextpdf:layout:8.0.2")
+    implementation("com.itextpdf:bouncy-castle-adapter:8.0.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
 tasks.withType<KotlinCompile> {
     compilerOptions {
-        languageVersion.set(KOTLIN_1_9)
-        apiVersion.set(KOTLIN_1_9)
-        progressiveMode.set(true)
-        jvmTarget.set(JVM_17)
+        languageVersion = KOTLIN_1_9
+        apiVersion = KOTLIN_1_9
+        progressiveMode = true
+        jvmTarget = JVM_21
         freeCompilerArgs.addAll("-Xjvm-default=all", "-Xlambdas=indy", "-Xjsr305=strict", "-Xtype-enhancement-improvements-strict-mode", "-Xassertions=jvm")
     }
 }
