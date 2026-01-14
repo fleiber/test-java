cc5b7d82b8b40061e25399325d264beee8ae041d
François LEIBER <fleiber@gmail.com>
2026-01-14 18:02:26 +0100

    Upgrade dependencies, including Kotlin 2.3.0 and Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
     id("com.github.ben-manes.versions") version "0.53.0" // find dependency updates
 }
 
@@ -17,27 +15,27 @@ dependencies {
     api("com.google.guava:guava:33.5.0-jre")
     api("org.apache.commons:commons-csv:1.14.1")
 
-    implementation("ch.qos.logback:logback-classic:1.5.20")
+    implementation("ch.qos.logback:logback-classic:1.5.24")
     implementation("com.github.luben:zstd-jni:1.5.7-6")
 
-    implementation("com.itextpdf:kernel:9.3.0")
-    implementation("com.itextpdf:io:9.3.0")
-    implementation("com.itextpdf:layout:9.3.0")
-    implementation("com.itextpdf:bouncy-castle-adapter:9.3.0")
+    implementation("com.itextpdf:kernel:9.5.0")
+    implementation("com.itextpdf:io:9.5.0")
+    implementation("com.itextpdf:layout:9.5.0")
+    implementation("com.itextpdf:bouncy-castle-adapter:9.5.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.2")
 }
 
-tasks.withType<KotlinCompile> {
+kotlin {
+    jvmToolchain(25)
     compilerOptions {
-        languageVersion = KOTLIN_2_2
-        apiVersion = KOTLIN_2_2
+        languageVersion = KOTLIN_2_3
+        apiVersion = KOTLIN_2_3
         progressiveMode = true
-        jvmTarget = JVM_21
         extraWarnings = true
         jvmDefault = JvmDefaultMode.NO_COMPATIBILITY
-        freeCompilerArgs.addAll("-Xjvm-default=all", "-Xjsr305=strict", "-Xtype-enhancement-improvements-strict-mode", "-Xassertions=jvm")
+        freeCompilerArgs.addAll("-Xwhen-expressions=indy", "-Xjsr305=strict", "-Xtype-enhancement-improvements-strict-mode", "-Xassertions=jvm")
     }
 }
 
